8d71922803ac1ddf56e2d27f2bd339e2fb66c156
Josh Stewart <josh.stewart@awesomeinc.org>
2022-03-08 02:00:11 +0000

    tic tac toe

---
+++ controller.js
@@ -0,0 +1,27 @@
+class GameController {
+  constructor() {
+    this.model = new TicTacToe();
+    this.view = new View();
+
+    this.view.playEvent.addListener(move => {
+      this.model.play(move);
+    });
+
+    this.model.updateCellEvent.addListener(data => {
+      this.view.updateCell(data);
+    });
+
+    this.model.victoryEvent.addListener(winner => {
+      this.view.victory(winner);
+    });
+
+    this.model.drawEvent.addListener(() => {
+      this.view.draw();
+    });
+
+  }
+
+  run() {
+    this.view.render();
+  }
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+const app = new GameController();
+
+app.run();
